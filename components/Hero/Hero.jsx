14362d53c22b93cd6ad31b05389821ba54b55765
unknown <rashidul537@gmail.com>
2023-06-30 00:12:23 +0600

    final

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -41,30 +41,63 @@ const Hero = () => {
         </section>
 
         <section className="relative justify-end flex-1 hidden lg:flex ">
-          <div className="w-[300px] h-[420px] absolute rounded-[24px] translate-y-[20px] ">
-            <Image
-              fill
-              src="/assets/heroimage3.png"
-              alt="herobg3"
-              loading="lazy"
-            />
-          </div>
-          <div className="w-[350px] h-[420px] absolute rounded-[24px] translate-x-[-30px] translate-y-[10px]">
-            <Image
-              fill
-              src="/assets/heroimage2.png"
-              alt="herobg2"
-              loading="lazy"
-            />
-          </div>
+          <Image
+            className="absolute rounded-[24px] translate-y-[20px] "
+            width={300}
+            height={420}
+            src="/assets/heroimage3.png"
+            alt="herobg3"
+            loading="lazy"
+          />
 
-          <div className="w-[400px] h-[420px] absolute rounded-[24px] translate-x-[-60px] ">
+          <Image
+            className="absolute   rounded-[24px] translate-x-[-30px] translate-y-[10px]"
+            width={350}
+            height={420}
+            src="/assets/heroimage2.png"
+            alt="herobg2"
+            loading="lazy"
+          />
+          <div className="relative w-[400px] h-[420px]">
+            <div className="z-20 w-[101px]  translate-x-[-110px] translate-y-[170px] absolute h-[101px] rotate-[30deg] rounded-full bg-[#FFE0D4]"></div>
             <Image
-              fill
+              className="absolute rounded-[24px] translate-x-[-60px] "
+              width={400}
+              height={420}
               src="/assets/heroimage1.png"
               alt="herobg1"
               loading="lazy"
             />
+            <div className="absolute flex flex-col justify-between  gap-5 px-5 py-5 w-full h-full  translate-x-[-60px] ">
+              <div className="flex flex-col gap-5">
+                <h2 className="text-[#fff] text-[28px] font-bold ">
+                  Abstr Gradient NFT
+                </h2>
+                <div className="flex gap-5">
+                  <div className="w-[32px] h-[32px] relative">
+                    <Image
+                      className="absolute"
+                      fill
+                      src="/assets/avatar1.png"
+                    />
+                  </div>
+                  <h2 className="text-[#fff] text-[20px] font-bold  ">
+                    Arkhan17
+                  </h2>
+                </div>
+              </div>
+
+              <div className="flex rounded-xl p-5 justify-between bg-[#ffffff33] backdrop-blur-xl text-[#fff]">
+                <div>
+                  <p>Current Bid</p>
+                  <p className="font-bold">0.25 ETH</p>
+                </div>
+                <div>
+                  <p>Ends in</p>
+                  <p>12h 43m 42s</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
